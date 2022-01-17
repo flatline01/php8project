349f5395fe10d50f198dbb1c080802296559c984
Joe Porto <cthulhu0@gmail.com>
2022-01-16 19:59:30 -0500

    add dotenv

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 use Flatline\Testing\Template as Template;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$f = new Template();
-echo $f->header();
+$t = new Template();
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo $t->header();
+var_dump($_ENV);
 ?>
 
 <h1>hiii</h1>
 
 <?php
-echo $f->footer();
+echo $t->footer();
 ?>
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # PHP 8 composer / autoloader
 
 Testing some stuff.
+
+https://www.specbee.com/blogs/introduction-php-standard-recommendation-psr
+https://stackoverflow.com/questions/68408564/how-to-use-phpdotenv-library-on-my-whole-project
